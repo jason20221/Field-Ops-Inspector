9b089633d0d5adf02d78a896fca96c7e484e13b6
jason20221 <jason20221@gmail.com>
2026-08-22 06:07:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='field-ops-v5';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./manifest.json','./Sinewave-logo.png'])));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));return;}event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
