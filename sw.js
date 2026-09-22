60916df1063a291b9bb37577977e6df705558978
jason20221 <jason20221@gmail.com>
2026-09-22 07:08:54 -0500

    Polish interface and open logging by default

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='field-ops-v25';
+const CACHE='field-ops-v26';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys()
