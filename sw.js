7c110db0ea65da45e86b4eea045f222b1fb5ef29
jason20221 <jason20221@gmail.com>
2026-09-22 07:57:45 -0500

    Add full asset list and Field Ops branding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='field-ops-v26';
+const CACHE='field-ops-v27';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys()
